4d880f967bff483a15c520b84d78afd79e192cff
slavik <slavayus@gmail.com>
2018-03-04 12:59:10 +0300

    1726 done

--- study/second/1726/main.cpp
+++ study/second/1726/main.cpp
@@ -1,40 +1,59 @@
 #include <iostream>
-#include <vector>
+
+void quickSort(unsigned long *data, long long start, long long end);
 
 using namespace std;
 
 int main() {
 
-    int numberOfMembers;
+    long long numberOfMembers;
     cin >> numberOfMembers;
 
-    auto *right = new int[numberOfMembers];
-    auto *down = new int[numberOfMembers];
+    auto *right = new unsigned long[numberOfMembers];
+    auto *down = new unsigned long[numberOfMembers];
 
-    for (int i = 0; i < numberOfMembers; ++i) {
+    for (long long i = 0; i < numberOfMembers; ++i) {
         cin >> right[i];
         cin >> down[i];
     }
 
+    quickSort(right, 0, numberOfMembers - 1);
+    quickSort(down, 0, numberOfMembers - 1);
 
-    auto *roadLength = new long long[numberOfMembers];
-    for (int i = 0; i < numberOfMembers; ++i) {
-        roadLength[i] = 0;
+    unsigned long long int length = 0;
+    for (long long i = 0; i < numberOfMembers - 1; ++i) {
+        length += (right[i + 1] - right[i] + down[i + 1] - down[i]) * (i + 1) * (numberOfMembers - i - 1);
     }
 
+    cout << (length * 2) / (numberOfMembers * (numberOfMembers - 1)) << endl;
 
-    for (int j = 0; j < numberOfMembers; ++j) {
-        for (int i = j + 1; i < numberOfMembers; ++i) {
-            long long length = abs(right[i] - right[j]) + abs(down[i] - down[j]);
-            roadLength[i] += length;
-            roadLength[j] += length;
+    return 0;
+}
+
+void quickSort(unsigned long *data, long long start, long long end) {
+
+    long long i = start;
+    long long j = end;
+
+    unsigned long middle = data[(start + end) / 2];
+
+    do {
+        while (data[i] < middle) {
+            i++;
         }
+        while (data[j] > middle) {
+            j--;
+        }
+        if (i <= j) {
+            swap(data[i++], data[j--]);
+        }
+    } while (i <= j);
+
+    if (i < end) {
+        quickSort(data, i, end);
     }
 
-    long long fullRoad = 0;
-    for (int i = 0; i < numberOfMembers; ++i) {
-        fullRoad += roadLength[i];
+    if (j > start) {
+        quickSort(data, start, j);
     }
-
-    cout << fullRoad / (numberOfMembers*(numberOfMembers-1)) << endl;
 }
