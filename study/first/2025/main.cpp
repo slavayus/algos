91d70aeacac18c4919eff9f48c0c581e9396482e
slavik <slavayus@gmail.com>
2018-03-14 16:12:01 +0300

    added comments to 2025 task

--- study/first/2025/main.cpp
+++ study/first/2025/main.cpp
@@ -7,6 +7,7 @@ using std::cin;
 using std::vector;
 
 int main() {
+    //считываем количество тестов
     int testCount;
     cin >> testCount;
 
@@ -18,17 +19,25 @@ int main() {
     int personInOneTeam;
     vector<int> answers;
 
+    //Запускам алгоритм для каждого тестка
     for (int i = 0; i < testCount; ++i) {
         cin >> n;
         cin >> k;
 
+        //Посчитали количество бойцов в одной команаде
         personInOneTeam = n / k;
 
+        //Но не все команды могут получится с одинаковым коливеством бойцов.
+        //Вычисляем количество команд, в составе которых больше бойцов
         countLargeTeam = n - (personInOneTeam) * k;
 
+        //Количество схваток между бойцами
         duelCount = 0;
 
+        //Берем текущую команду и умножаем на количество оставшихся бойцов.
+        //Постоянно уменьшаем количество бойцов, чтобы дважды не считать один и тот же бой.
         while (n > 0) {
+            //Пока не закончились команды, в составе которых больше бойцов, вычисляем число бойцов в текущей команде
             personInCurrentTeam = countLargeTeam > 0 ? personInOneTeam + 1 : personInOneTeam;
 
             --countLargeTeam;
@@ -37,6 +46,7 @@ int main() {
 
             duelCount += personInCurrentTeam * n;
         }
+
         answers.push_back(duelCount);
     }
 
